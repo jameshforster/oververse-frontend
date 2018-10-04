239e33d728526a756fbf354a72192368969cc262
James Forster <james.forster@digital.hmrc.gov.uk>
2018-10-04 10:05:28 +0100

    Added view tests for the star map.

---
+++ server/test/views/universe/StarMapViewSpec.scala
@@ -0,0 +1,68 @@
+package views.universe
+
+import models.{StarSystem, UserDetailsModel}
+import specs.UnitSpec
+import helpers.EntityHelper._
+import models.location.Coordinates
+import org.jsoup.Jsoup
+import views.html.universe.starMap
+
+class StarMapViewSpec extends UnitSpec {
+
+  "The star map" should {
+
+    "generate the map with the correct number of rows and columns" when {
+
+      "provided with any star system" in {
+        val starSystem = StarSystem(testStar, Seq(), Seq(), Seq())
+        val doc = starMap(starSystem, UserDetailsModel("name", "email", 100))(fakeRequest)
+        val body = Jsoup.parse(doc.body)
+
+        body.select("tr").size() shouldBe 17
+        for (x <- 0 to 16) {
+          body.select("tr").get(x).select("td").size() shouldBe 17
+        }
+      }
+    }
+
+    "load an image for a star at the origin point" when {
+
+      "provided with any star system" in {
+        val starSystem = StarSystem(testStar, Seq(), Seq(), Seq())
+        val doc = starMap(starSystem, UserDetailsModel("name", "email", 100))(fakeRequest)
+        val body = Jsoup.parse(doc.body)
+
+        val star = body.select("tr").get(8).select("td").get(8).select("img")
+
+        body.select("img").size() shouldBe 1
+        star.attr("width") shouldBe "65%"
+        star.attr("src") shouldBe "/versionedAssets/images/redStar.png"
+        star.attr("onClick") shouldBe """parent.selectStar("galaxyName", 1, 2)"""
+      }
+    }
+
+    "load an image for any planets" when {
+
+      "provided with only a single planet" in {
+        val starSystem = StarSystem(testStar, Seq(testPlanet), Seq(), Seq())
+        val doc = starMap(starSystem, UserDetailsModel("name", "email", 100))(fakeRequest)
+        val body = Jsoup.parse(doc.body)
+
+        val planet = body.select("tr").get(4).select("td").get(11).select("img")
+
+        body.select("img").size() shouldBe 2
+        planet.attr("width") shouldBe "50%"
+        planet.attr("src") shouldBe "/versionedAssets/images/planet.png"
+        planet.attr("onClick") shouldBe """parent.selectPlanet("galaxyName", 1, 2 ,3, 4)"""
+      }
+
+      "provided with multiple planets" in {
+        val starSystem = StarSystem(testStar, Seq(testPlanet, testPlanet.copy(orbitalCoordinates = Coordinates(5, -3))), Seq(), Seq())
+        val doc = starMap(starSystem, UserDetailsModel("name", "email", 100))(fakeRequest)
+        val body = Jsoup.parse(doc.body)
+
+        body.select("img").size() shouldBe 3
+      }
+    }
+  }
+}
